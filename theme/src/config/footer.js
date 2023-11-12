845d55a61190253e1ade55a28087ded7a3823c34
Alejandro Ayerdi <ayerdialejandro.ing@gmail.com>
2023-11-12 15:24:01 +0100

    first commit

---
+++ theme/src/config/footer.js
@@ -0,0 +1,78 @@
+export const footerSocials = [
+
+]
+
+export const footerLists = [
+	{
+		title: ' ',
+		items: [
+			{
+				title: 'INICIO',
+				slug: '/',
+			},
+			{
+				title: 'CÓMO TRABAJO',
+				slug: '/metodo',
+			},
+			{
+				title: 'SERVICIOS',
+				slug: '/servicios',
+			},
+			{
+				title: 'PSICÓLOGO ONLINE',
+				slug: '/online',
+			} 
+			/*,
+			{
+				title: 'Como Trabajo',
+				slug: '/landing-pages/landing-2',
+			},
+			{
+				title: 'Servicios',
+				slug: '/landing-pages/landing-2',
+			},
+			{
+				title: 'Psicologo Online',
+				slug: '/landing-pages/landing-2',
+			}*/,
+			{
+				title: 'CONTACTO',
+				slug: '/contacto',
+			} /*,
+			{
+				title: 'Blog',
+				slug: '/landing-pages/landing-2',
+			},*/
+      // {
+			// 	title: 'Landing Page 3',
+			// 	slug: '/landing-pages/landing-3',
+			// },
+		],
+	}
+
+];
+
+
+/*
+	{
+		title: 'Theme',
+		items: [
+			{
+				title: 'Get Started',
+				slug: '/theme/get-started',
+			},
+			{
+				title: 'Style Guide',
+				slug: '/theme/style-guide',
+			},
+      {
+        title: 'Theme Setup',
+        slug: '/theme/theme-setup',
+      },
+      {
+        title: 'Customizing Odyssey',
+        slug: '/theme/customizing-odyssey',
+      }
+		],
+	},
+*/
---
+++ theme/src/config/nav.js
@@ -0,0 +1,25 @@
+export const nav = [
+
+	{
+		title: 'INICIO',
+		slug: '/',
+	},
+	{
+		title: 'CÓMO TRABAJO',
+		slug: '/metodo',
+	},
+	{
+		title: 'SERVICIOS',
+		slug: '/servicios',
+	},
+	{
+		title: 'PSICÓLOGO ONLINE',
+		slug: '/online',
+	},
+	{
+		title: 'CONTACTO',
+		slug: '/contacto',
+	},
+
+];
+
